843163399c1fa3e1732f1755ecbfd8f6acfe94a7
Debo <dwdeante0726@gmail.com>
2023-06-11 22:22:04 -0700

    This will add functions to Scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 window.onload = function() {
-document.getElementById("myBtn").addEventListener("click", function() {
-  alert("Hello World!");
-});
+  document.getElementById("myBtn").addEventListener("click", function keyDown() { 
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white'; 
+  });
+  document.getElementById("myBtn").addEventListener("dblclick", function keyDown() { 
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'black'; 
+  });
 }
